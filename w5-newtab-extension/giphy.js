558c7b9b8138628fa769d7b1527e16e4677960eb
asd0999 <obelixotic@gmail.com>
2019-03-04 16:26:33 -0500

    font size changes in w5 cat gif extension

--- w5-newtab-extension/giphy.js
+++ w5-newtab-extension/giphy.js
@@ -1,8 +1,8 @@
 let gifUrl; // 1ms
 let Urls = [];
-Urls.push("https://api.giphy.com/v1/gifs/search?api_key=Rj4NRE2qDVvjw7gAftCnEyjFkbMUqbXB&q=dogs&limit=30");
-Urls.push("https://api.giphy.com/v1/gifs/search?api_key=Rj4NRE2qDVvjw7gAftCnEyjFkbMUqbXB&q=cats&limit=30");
-Urls.push("https://api.giphy.com/v1/gifs/search?api_key=Rj4NRE2qDVvjw7gAftCnEyjFkbMUqbXB&q=cats-funny&limit=30");
+Urls.push("https://api.giphy.com/v1/gifs/search?api_key=Rj4NRE2qDVvjw7gAftCnEyjFkbMUqbXB&q=cats-in-space&limit=100");
+Urls.push("https://api.giphy.com/v1/gifs/search?api_key=Rj4NRE2qDVvjw7gAftCnEyjFkbMUqbXB&q=cats&limit=100");
+Urls.push("https://api.giphy.com/v1/gifs/search?api_key=Rj4NRE2qDVvjw7gAftCnEyjFkbMUqbXB&q=funny-cats&limit=100");
 let Url = Urls[Math.floor(Math.random() * Urls.length)];
 fetch(Url)
   .then(data => {
@@ -19,29 +19,11 @@ fetch(Url)
 setTimeout(function() {
   let currentDate = document.getElementById("date");
   let d = new Date();
-  // let year = d.getFullYear();
-  // let month = d.getMonth() + 1;
-  // let date = d.getDate();
-  // currentDate.innerHTML = `${date} ${month} ${year}`
   currentDate.innerHTML = d.toDateString();
   console.log(currentDate);
+}, 100);
+
+setTimeout(function() {
+  let meow = document.getElementById("meow");
+  meow.innerHTML = "Have a meow day!"
 }, 1000);
-
-// let gif = document.getElementById("mygif");
-// console.log(gif);
-// gif.setAttribute("src", gifUrl);
-// gif.src = gifUrl;
-
-
-
-
-
-// function callGiphy() {
-//   var url = "https://api.giphy.com/v1/gifs/search?api_key=Rj4NRE2qDVvjw7gAftCnEyjFkbMUqbXB&q=cats&limit=2";
-//   loadJSON(url, gotData);
-//
-//   function gotData(giphy) {
-//     var img = createImg(giphy.data[0].images.original.url);
-//     img.size(400, 400);
-//   }
-// }
