a91429c614696d42484db8789e22dc532163aa22
asd0999 <obelixotic@gmail.com>
2019-03-11 22:50:47 -0400

    Working prototype basic

--- w6-password-extension/background.js
+++ w6-password-extension/background.js
@@ -1,34 +1,38 @@
-let x = [{
-  url: 'default',
-  password: 'Password@12'
-}];
-// let found = false;
+//copy of background.js at prrof of concept stage as backup
+let x = {
+  "default": "Password@12"
+};
+let found = false;
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.message === "password_submitted") {
-      x.push(request.data);
-      console.log(x[x.length - 1]);
-      // found = false;
-      //vs i=0;i<x.length-1;i++ --> only checking the last entry with the rest
-      for (let i = x.length - 1; i <= x.length - 1; i--) {
-        let repeats = [];
-        for (let j = x.length - 1; j >= 0; j--) {
-          if (i != j && x[i].url != x[j].url && x[i].password == x[j].password) {
-            if (repeats.includes(x[j])) {
-              break;
-            } else {
-              repeats.push(x[j]);
-            }
-          }
-        }
-        sendResponse({
-          "repeats": repeats
-        });
-        console.log("password re-use detected!");
+    // console.log("message received");
+    let repeats = [];
+    x[request.data.url] = request.data.password;
+    console.log(x);
+    // if(Object.values(x).includes(request.data.password)){}
+    for (let [url, pw] of Object.entries(x)) {
+      if (url !== request.data.url && pw === request.data.password) {
+        // same password used on a diff url
+        repeats.push(url);
+        found = true;
       }
-      // if (found) break;
     }
-    return true;
+    if (found) {
+      // console.log("password re-use detected!");
+      let l = repeats.length;
+      if (l == 1) {
+        window.alert(`You used the same password on ${repeats[0]}. Consider changing one of the passwords to avoid #pwnage`);
+      } else if (l == 2) {
+        window.alert(`You used the same password on ${repeats[0]} and ${repeats[1]}. Consider changing one of the passwords to avoid #pwnage`);
+      } else if (l >= 3) {
+        window.alert(`You used the same password on ${repeats[0]}, ${repeats[1]} and ${repeats[2]}. Consider changing one of the passwords to avoid #pwnage`);
+      }
+      sendResponse({
+        "repeats": repeats
+      });
+    }
   }
 );
+
+//everytime new entry is added in object, run this to check duplicate entry
--- w6-password-extension/content.js
+++ w6-password-extension/content.js
@@ -15,60 +15,19 @@ function assignBlur(newInput) {
   console.log("assignBlur");
   $(newInput).blur(function() {
     console.log("blur done");
-    console.log(newInput);
+    // console.log(newInput);
     var password = $(this).val();
     x = {
       url: window.location.hostname,
       password: password
     };
-    // chrome.runtime.sendMessage(password);
+
     chrome.runtime.sendMessage({
-      "message": "password_submitted",
       "data": x
     }, function(request) {
       console.log("response from sendMessage:", request);
-      let l = request.repeats.length;
-      console.log(l);
-      if (l == 1) {
-        window.alert(`You used the same password on ${request.repeats[0].url}`);
-      } else if (l == 2) {
-        window.alert(`You used the same password on ${request.repeats[0].url} and ${request.repeats[1].url}`);
-      } else if (l >= 3) {
-        window.alert(`You used the same password on ${request.repeats[0].url}, ${request.repeats[1].url} and ${request.repeats[2].url}`);
-      }
-      // let a = request.host1;
-      // let b = request.host2;
-      // window.alert(`You used the same password on ${b}. Consider changing one of the passwords to avoid being a victim of credential stuffing attacks! #nopwn`);
     });
   });
 }
 
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//   console.log("recd message");
-//   let a = request.host1;
-//   let b = request.host2;
-//   window.alert(`you used the same password for ${a} and ${b}`);
-//   // giveAlert();
-// });
-
-// function giveAlert() {
-//   window.alert(`you used the same password for ${} and ${}`);
-// }
-
 setInterval(doThis, 500);
-
-// setInterval(function() {
-//   var insertedNodes = [];
-//   var observer = new MutationObserver(function(mutations) {
-//     mutations.forEach(function(mutation) {
-//       for (var i = 0; i < mutation.addedNodes.length; i++)
-//         insertedNodes.push(mutation.addedNodes[i]);
-//     })
-//   });
-//   observer.observe(document, {
-//     childList: true
-//   });
-// console.log(insertedNodes);
-// }, 1000);
-
-//not working on udemy and udacity - is it an autofill thing???
