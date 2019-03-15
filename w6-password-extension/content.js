15aebcbf5b228e2a90e54944b28a752d414c3cb2
asd0999 <obelixotic@gmail.com>
2019-03-15 01:15:26 -0400

    with password hashing SHA256

---
+++ w6-password-extension/content.js
@@ -0,0 +1,36 @@
+function doThis() {
+  let inputs = $('input[type=password]');
+  inputs.each(function(i, input) {
+    if ($(input).data("seen") == true) {
+      return;
+    } else {
+      console.log("found a new input field password");
+      $(input).data("seen", true);
+      assignBlur(input);
+    }
+  })
+}
+
+function assignBlur(newInput) {
+  console.log("assignBlur");
+  $(newInput).blur(function() {
+    console.log("blur done");
+    // console.log(newInput);
+    var password = $(this).val();
+    var salt = "y0u_n33d_$4lt_2_m4k3_L3m0n4d3"
+    var SHA256 = new Hashes.SHA256;
+    var passwordHash = SHA256.hex_hmac(password, salt)
+    x = {
+      url: window.location.hostname,
+      password: passwordHash
+    };
+
+    chrome.runtime.sendMessage({
+      "data": x
+    }, function(request) {
+      console.log("response from sendMessage:", request);
+    });
+  });
+}
+
+setInterval(doThis, 300);
